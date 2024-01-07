25e82e61a970a3150519c233953735dbfd910292
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2024-01-07 19:52:22 +0000

    [LeetCode Sync] Runtime - 35 ms (49.20%), Memory - 14 MB (16.13%)

---
+++ leetcode-solutions/0001-two-sum/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {} # This dictionary will keep track of numbers (as key) and indices
+       
+        for i, value in enumerate(nums): #1
+           remaining = target - nums[i] #2
+           
+           if remaining in seen: #3
+               return [i, seen[remaining]]  #4
+           else:
+               seen[value] = i  #5
