0e0310f66892687effe1f6a2b048befe6cedf989
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2024-05-28 14:34:37 +0000

    [LeetCode Sync] Runtime - 99 ms (5.13%), Memory - 22.2 MB (34.90%)

--- leetcode-solutions/0049-group-anagrams/solution.py
+++ leetcode-solutions/0049-group-anagrams/solution.py
@@ -1,55 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
-        anagrams = {}
+        res = defaultdict(list) 
 
         for s in strs:
-            sorted_s = ''.join(sorted(s))
+            count = [0] * 26 #a...z - array for the 26 charecters
 
-            if sorted_s not in anagrams:
-                anagrams[sorted_s] = [s]
+            for c in s:
+                count[ord(c) - ord("a")] += 1
 
-            else:
-                anagrams[sorted_s].append(s)
+            res[tuple(count)].append(s)
 
-        return list(anagrams.values())
-
-        # -----------------------APPROACH 1-----------
-
-        # dic={}
-        # k=[]
-
-        # for i in strs:
-        #     l="".join(sorted(i))
-        #     dic.setdefault(l,[]).append(i)
-
-        # return dic.values()
-
-        # --------------- APPROACH 2 --------------------
-
-        # anagrams = defaultdict(list)
-
-        # for i, v in enumerate(strs):
-        #     sorted_str = ''.join(sorted(v))
-        #     anagrams[sorted_str].append(v)
-        # return list(anagrams.values())
-
-        # -----------------APPROACH 3-----------------------------
-
-    # def groupAnagrams(strs):
-        # anagrams = {} # Initialize the hash table
-        
-        # for s in strs:
-        #     # Sort the characters of the string and use it as a key
-        #     sorted_s = ''.join(sorted(s))
-            
-        #     # If the sorted string is not in the hash table, add it
-        #     if sorted_s not in anagrams:
-        #         anagrams[sorted_s] = [s]
-        #     else:
-        #         # If it is in the hash table, append the original string to the corresponding list
-        #         anagrams[sorted_s].append(s)
-        
-        # # Return the values of the hash table, which are lists of anagrams
-        # return list(anagrams.values())
-
+        return res.values()
