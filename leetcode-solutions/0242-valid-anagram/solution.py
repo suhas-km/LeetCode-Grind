900cb514a826d61ea62a3266eb0a88e3b2cedee2
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2024-07-01 18:25:13 +0000

    [LeetCode Sync] Runtime - 31 ms (5.47%), Memory - 16.8 MB (100.00%)

--- leetcode-solutions/0242-valid-anagram/solution.py
+++ leetcode-solutions/0242-valid-anagram/solution.py
@@ -1,10 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        sorted_s = sorted(s)
-        sorted_t = sorted(t)
-
-        if sorted_s == sorted_t:
-            return True
-        
-        else:
+        #if the length of both strings aren't equal then they arent anagrams. return false.
+        if len(s)!=len(t): 
             return False
+        
+        init=set();#declare a new set
+        
+        for letter in s:#for every letter in the string s
+            if letter not in init:#if the letter is not there in the new set
+                init.add(letter)#add the letter to the new set
+                
+                if s.count(letter)!=t.count(letter): 
+                    return False
+                #if the no of letters in not the same in both strings s and t, return False.
+                #else return True.
+        return True
