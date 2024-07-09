4581b79438a503ad3b49b285a5b3fbb06086a6e5
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2024-07-09 13:09:41 +0530

    Time: 111 ms (16.37%), Space: 17.4 MB (86.92%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -20,16 +20,18 @@ class Solution:
                     else:
                         rowZero = True
         
+        # Then we zero out most of them
         for r in range(1, ROWS):
             for c in range(1, COLS):
                 if matrix[0][c] == 0 or matrix[r][0] == 0:
                     matrix[r][c] = 0
         
-        
+        # then we zero out the first column if we need to!
         if matrix[0][0] == 0:
             for r in range(ROWS):
                 matrix[r][0] = 0
                 
+        # Again if we need to we will zero out the first row!
         if rowZero:
             for c in range(COLS):
                 matrix[0][c] = 0
