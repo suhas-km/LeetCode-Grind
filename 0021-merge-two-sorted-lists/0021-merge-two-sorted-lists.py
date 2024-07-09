085509fc4a5eaad44a1b8298553843265a786aac
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2024-07-09 10:33:01 +0530

    Time: 45 ms (14.17%), Space: 16.5 MB (76.47%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -7,16 +7,32 @@
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        cur = dummy = ListNode()
-        while list1 and list2:               
+        # Create a dummy node to serve as the starting point of the merged list
+        dummy = ListNode()
+        # Tail will be used to keep track of the end of the merged list
+        tail = dummy
+
+        # Iterate while both list1 and list2 are not None
+        while list1 and list2:
+            # Compare the current nodes of both lists
             if list1.val < list2.val:
-                cur.next = list1
-                list1, cur = list1.next, list1
+                # If the value in list1 is smaller, append it to the merged list
+                tail.next = list1
+                # Move to the next node in list1
+                list1 = list1.next
             else:
-                cur.next = list2
-                list2, cur = list2.next, list2
-                
-        if list1 or list2:
-            cur.next = list1 if list1 else list2
-            
+                # If the value in list2 is smaller or equal, append it to the merged list
+                tail.next = list2
+                # Move to the next node in list2
+                list2 = list2.next
+            # Move the tail to the last node added
+            tail = tail.next
+
+        # If one of the lists is exhausted, append the remaining nodes of the other list
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+
+        # Return the next node of dummy since dummy is a placeholder
         return dummy.next
