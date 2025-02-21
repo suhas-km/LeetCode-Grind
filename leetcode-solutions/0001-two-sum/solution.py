dac32360e27791d2d36d33f2abebaae6eb3dfa9e
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-02-21 03:27:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.9 MB (34.39%)

--- leetcode-solutions/0001-two-sum/solution.py
+++ leetcode-solutions/0001-two-sum/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {} # dictionary storing all the seen values of the list
+        seen = {}
 
-        for i, value in enumerate(nums): 
-            remaining = target - nums[i] #finding the remainnig value to be located in seen {}
+        for i, value in enumerate(nums):
+            remaining = target - nums[i]
 
             if remaining in seen:
-                return [i, seen[remaining]] #if remaining found, Solution reached
+               return [i, seen[remaining]]
+            
             else:
-                seen[value] = i # else we append the value to the list
-                
+                seen[value] = i
