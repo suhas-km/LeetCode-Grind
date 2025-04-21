7edf620724e190935fd209b9f9bb012ec9c72cac
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-04-21 04:50:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.8 MB (63.04%)

--- leetcode-solutions/0001-two-sum/solution.py
+++ leetcode-solutions/0001-two-sum/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
+        seen = {} # keep map of seen values.
 
         for i, value in enumerate(nums):
-            remaining = target - nums[i]
+
+            remaining = target - nums[i] # x + y = z
 
             if remaining in seen:
-               return [i, seen[remaining]]
-            
+                return i, seen[remaining]
             else:
-                seen[value] = i
+                seen[value] = i #seen.add(i)
+                continue
