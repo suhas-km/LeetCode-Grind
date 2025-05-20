8271b304958ce30ad2c6b13092ee8d1316b05f74
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-05-20 04:28:31 +0000

    [LeetCode Sync] Runtime - 537 ms (74.62%), Memory - 20.7 MB (37.78%)

--- leetcode-solutions/0015-3sum/solution.py
+++ leetcode-solutions/0015-3sum/solution.py
@@ -1,23 +1,24 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()  # Sort the array
-        deduplicate = set()  # Using a set to avoid duplicates
-        
-        for i in range(len(nums) - 2):
-            start, end = i + 1, len(nums) - 1  # Two pointers for each 'i'
-            while start < end:
-                current_sum = nums[i] + nums[start] + nums[end]
-                if current_sum == 0:
-                    # If we found a triplet, add it to the set to avoid duplicates
-                    deduplicate.add((nums[i], nums[start], nums[end]))
-                    start += 1
-                    end -= 1
-                elif current_sum < 0:
-                    # If sum is less than zero, move the start pointer to increase sum
-                    start += 1
+        res = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i -1]: # possible first value and avoiding reusing old value
+                continue # continue to next number if so (Skip to next)
+            
+            # two-sum logic from here - sorted array
+            l, r = i + 1, len(nums) - 1 
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
                 else:
-                    # If sum is greater than zero, move the end pointer to decrease sum
-                    end -= 1
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l<r:
+                        l+=1
         
-        # Convert the set of triplets into a list of lists and return
-        return [list(triplet) for triplet in deduplicate]
+        return res
