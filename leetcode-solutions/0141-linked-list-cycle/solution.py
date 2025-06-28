6ead55f2903ae97020b2078a1bc8e22b66dfe780
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-06-28 15:13:20 +0000

    [LeetCode Sync] Runtime - 47 ms (66.94%), Memory - 19.9 MB (30.27%)

--- leetcode-solutions/0141-linked-list-cycle/solution.py
+++ leetcode-solutions/0141-linked-list-cycle/solution.py
@@ -6,11 +6,11 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow_pointer = head
-        fast_pointer = head
-        while fast_pointer and fast_pointer.next:
-            slow_pointer = slow_pointer.next
-            fast_pointer = fast_pointer.next.next
-            if slow_pointer == fast_pointer:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
                 return True
         return False
