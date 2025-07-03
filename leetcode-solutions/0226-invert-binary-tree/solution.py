528c94659aaa147b85b4cd97f09d0b5bb8279413
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-07-03 18:05:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.6 MB (96.68%)

--- leetcode-solutions/0226-invert-binary-tree/solution.py
+++ leetcode-solutions/0226-invert-binary-tree/solution.py
@@ -6,12 +6,15 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root: #Base Case
-            return root
+        if not root: # base case
+            return None
         
-        self.invertTree(root.left) #Call the left substree
-        self.invertTree(root.right)  #Call the right substree
-        # Swap the nodes
-        root.left, root.right = root.right, root.left
-        return root # Return the root
+        # swap the children
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
 
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
