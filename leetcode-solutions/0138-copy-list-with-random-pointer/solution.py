dbc6cb72bb828045998b0ef77e6da34f84012c14
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-07-20 03:55:44 +0000

    [LeetCode Sync] Runtime - 39 ms (73.18%), Memory - 18.4 MB (84.58%)

--- leetcode-solutions/0138-copy-list-with-random-pointer/solution.py
+++ leetcode-solutions/0138-copy-list-with-random-pointer/solution.py
@@ -10,18 +10,20 @@ class Node:
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         realToCopy = { None: None}
-
         cur = head
+        
+        # pass 1: create the nodes values in the hashmap
         while cur:
             copy = Node(cur.val)
             realToCopy[cur] = copy
             cur = cur.next
         
         cur = head
+        #pass 2: connect deepcopy to next and random
         while cur:
             copy = realToCopy[cur]
             copy.next = realToCopy[cur.next]
             copy.random = realToCopy[cur.random]
             cur = cur.next
-
+        
         return realToCopy[head]
