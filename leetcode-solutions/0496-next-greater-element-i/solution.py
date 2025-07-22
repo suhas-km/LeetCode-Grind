f49e52919cdef8eda37883014bca1984f96bd1d9
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-07-22 03:52:37 +0000

    [LeetCode Sync] Runtime - 19 ms (23.65%), Memory - 17.9 MB (66.11%)

--- leetcode-solutions/0496-next-greater-element-i/solution.py
+++ leetcode-solutions/0496-next-greater-element-i/solution.py
@@ -1,17 +1,17 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        stack = []
+
         res = [-1] * len(nums2)
+        stack = []
 
-        for i, v in enumerate(nums2): # creating monotonous stack for reference
+        for i, v in enumerate(nums2):
             while stack and v > stack[-1][1]:
-                stackInd, stackValue = stack.pop()
-                res[stackInd] = v
-
+                stackIndex, stackVal = stack.pop()
+                res[stackIndex] = v
             stack.append([i, v])
         
         res2 = []
         for i in nums1:
             res2.append(res[nums2.index(i)])
-    
+
         return res2
