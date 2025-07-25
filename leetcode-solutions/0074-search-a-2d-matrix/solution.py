197d6bea78b69fa484d75cc57cda2493ea73bbc9
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-07-25 14:27:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.5 MB (5.75%)

--- leetcode-solutions/0074-search-a-2d-matrix/solution.py
+++ leetcode-solutions/0074-search-a-2d-matrix/solution.py
@@ -1,30 +1,33 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        ROWS, COLS = len(matrix), len(matrix[0])
-        top, bot = 0, ROWS - 1
+        # Since the numbers are sorted we can perform binary search
+        # 2 passes: 1st to find the right row to search and next to find if target is there
+        # pass 1: O(log(m))
+        # pass 2: O(log(n))
+        # total time complexity: O(log(m + n))
 
+        ROWS, COLS = len(matrix), len(matrix[0])
+        
+        top, bot = 0, ROWS - 1
         while top <= bot:
             row = (top + bot) // 2
             if target > matrix[row][-1]:
                 top = row + 1
             elif target < matrix[row][0]:
-                bot = row - 1
+                bot = row -1
             else:
                 break
+        
+        if not (top <= bot): return False
 
-        # if not (top <= bot):
-        #     return False
-
-        row = (top + bot) // 2
         l, r = 0, COLS - 1
-
         while l <= r:
             m = (l + r) // 2
-            if target < matrix[row][m]:
-                r = m - 1
-            elif target > matrix[row][m]:
+            if target > matrix[row][m]:
                 l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
             else:
                 return True
-
+        
         return False
