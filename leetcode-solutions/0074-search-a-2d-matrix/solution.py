72392a9e6b100fea43966df81c03c8325c7e32f3
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-07-25 04:22:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.3 MB (25.78%)

--- leetcode-solutions/0074-search-a-2d-matrix/solution.py
+++ leetcode-solutions/0074-search-a-2d-matrix/solution.py
@@ -1,13 +1,30 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        n=len(matrix)
-        for i in range(n):
-            if target==matrix[i][0]:
-                return True
-            if target<matrix[i][0]:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        top, bot = 0, ROWS - 1
+
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
                 break
-        if target in matrix[i-1]:
-            return True
-        if target in matrix[n-1]:
-            return True
+
+        # if not (top <= bot):
+        #     return False
+
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if target < matrix[row][m]:
+                r = m - 1
+            elif target > matrix[row][m]:
+                l = m + 1
+            else:
+                return True
+
         return False
