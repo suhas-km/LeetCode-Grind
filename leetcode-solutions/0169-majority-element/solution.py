7bd04e4db3413d3c8e63cdb7aadf60f3c48dae0c
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-07-28 14:52:29 +0000

    [LeetCode Sync] Runtime - 10 ms (30.93%), Memory - 19.3 MB (50.79%)

--- leetcode-solutions/0169-majority-element/solution.py
+++ leetcode-solutions/0169-majority-element/solution.py
@@ -1,10 +1,21 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # Count the occurrences of each number
-        count = Counter(nums)
+        # APPROACH 1
+        count = 0
+        candidate = None
 
-        # Find the element with count > len(nums) // 2
-        for num, freq in count.items():
-            if freq > len(nums) // 2:
-                return num
-                
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if candidate == num:
+                count += 1
+            else:
+                count -= 1
+        
+        return candidate
+
+        # APPROACH 2
+        # The intuition behind this approach is that if an element occurs more than n/2 times in the array (where n is the size of the array), it will always occupy the middle position when the array is sorted.
+        # nums.sort()
+        # n = len(nums)
+        # return nums[n//2]
