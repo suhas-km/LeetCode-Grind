cce90fd33ca23f10d0b6efd7181d5d301c3e7f57
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-07-30 14:04:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.1 MB (9.94%)

--- leetcode-solutions/0020-valid-parentheses/solution.py
+++ leetcode-solutions/0020-valid-parentheses/solution.py
@@ -1,15 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = [] # create an empty stack to store opening brackets
-        for c in s: # loop through each character in the string
-            if c in '([{': # if the character is an opening bracket
-                stack.append(c) # push it onto the stack
-            else: # if the character is a closing bracket
+        stack = []
+
+        for c in s:
+            if c in '({[':
+                stack.append(c)
+            else:
                 if not stack or \
-                    (c == ')' and stack[-1] != '(') or \
-                    (c == '}' and stack[-1] != '{') or \
-                    (c == ']' and stack[-1] != '['):
-                    return False # the string is not valid, so return false
-                stack.pop() # otherwise, pop the opening bracket from the stack
-        return not stack # if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
-                         # so the string is valid, otherwise, there are unmatched opening brackets, so return false
+                   (c == ')' and stack[-1] != '(') or \
+                   (c == '}' and stack[-1] != '{') or \
+                   (c == ']' and stack[-1] != '['):
+                    return False
+                stack.pop()
+
+        return not stack
+
