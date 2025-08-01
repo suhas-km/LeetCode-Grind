cd3bb3144c5c45c11f1887a729b2b48edaf27f3a
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-08-01 03:57:44 +0000

    [LeetCode Sync] Runtime - 23 ms (18.30%), Memory - 22.7 MB (25.74%)

--- leetcode-solutions/0049-group-anagrams/solution.py
+++ leetcode-solutions/0049-group-anagrams/solution.py
@@ -1,17 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = {} # Initialize the hash table
+        anagram = defaultdict(list)
         
         for s in strs:
-            # Sort the characters of the string and use it as a key
-            sorted_s = ''.join(sorted(s))
-            
-            # If the sorted string is not in the hash table, add it
-            if sorted_s not in anagrams:
-                anagrams[sorted_s] = [s]
-            else:
-                # If it is in the hash table, append the original string to the corresponding list
-                anagrams[sorted_s].append(s)
-        
-        # Return the values of the hash table, which are lists of anagrams
-        return list(anagrams.values())
+            key = [0] * 26
+            for ch in s:
+                key[ord(ch) - ord("a")] += 1
+            anagram[tuple(key)].append(s)
+
+        res = []
+
+        for _, value in anagram.items():
+            res.append(value)
+
+        return res
