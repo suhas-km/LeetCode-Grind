85d27b27510301484801fad1a9a7f1ce241d072d
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-08-04 23:55:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (14.24%)

---
+++ leetcode-solutions/3226-minimum-number-game/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        """
+        nums = [1,2,3] -> int, even length
+        arr = []
+        Remove - alice and bob remove the smallest element, in the same order
+        Append - Bob appends first followed by alice, until nums is empty
+
+        """
+
+        nums.sort()
+        arr = []
+        a, b = 0, 1
+
+        # for i in range(0, len(nums), 2):
+        while a < len(nums) and b < len(nums):
+            arr.append(nums[b])
+            arr.append(nums[a])
+            a += 2
+            b += 2
+        
+        return arr
