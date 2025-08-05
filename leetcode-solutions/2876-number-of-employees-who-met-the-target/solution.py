4c25e47183a7867a6ab962ed86036cf512c0c0a3
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-08-05 03:57:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (32.48%)

---
+++ leetcode-solutions/2876-number-of-employees-who-met-the-target/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        count = 0
+
+        for hour in hours:
+            if hour >= target:
+                count += 1
+            continue
+        return count
