965a4b8275c2c18947fc7e9f1437e89a90438b2f
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-08-17 22:00:49 +0000

    [LeetCode Sync] Runtime - 11 ms (36.06%), Memory - 17.8 MB (64.39%)

--- leetcode-solutions/0009-palindrome-number/solution.py
+++ leetcode-solutions/0009-palindrome-number/solution.py
@@ -1,13 +1,19 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0 or (x != 0 and x % 10 == 0):
+        # Handle negative numbers immediately as they can't be palindromes (unless specifically defined otherwise)
+        if x < 0:
             return False
 
-        reversed_num = 0
-        original = x
+        num_str = str(x)  # Convert the integer to a string
+        l = 0
+        r = len(num_str) - 1
 
-        while x > reversed_num:
-            reversed_num = reversed_num * 10 + x % 10
-            x //= 10
-
-        return x == reversed_num or x == reversed_num // 10
+        while l <= r:
+            if num_str[l] == num_str[r]:  # Access characters from the string
+                l += 1
+                r -= 1
+            
+            else: 
+                return False
+        
+        return True
