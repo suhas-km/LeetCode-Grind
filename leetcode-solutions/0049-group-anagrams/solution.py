5f05631da65df33ecc7823be96019c9c3188c70a
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-08-19 04:19:08 +0000

    [LeetCode Sync] Runtime - 19 ms (36.00%), Memory - 22.8 MB (5.06%)

--- leetcode-solutions/0049-group-anagrams/solution.py
+++ leetcode-solutions/0049-group-anagrams/solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = defaultdict(list)
+        res = []
 
         for word in strs:
-            key = [0] * 26
+            key = [0] * 26 #basically key for every word
             for ch in word:
-                key[ord(ch) - ord("a")] += 1
-            
+                key[ord(ch) - ord("a")] += 1 # creating the key properly for thew word
+            # appendd to anagrams
             anagrams[tuple(key)].append(word)
-        
-        res = []
+                
         for _, value in anagrams.items():
             res.append(value)
         
