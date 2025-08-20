c72d3f8a42b5e09f01b900e4e2bc4b83fed2b1cb
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-08-20 21:20:43 +0000

    [LeetCode Sync] Runtime - 38 ms (40.65%), Memory - 34.9 MB (64.33%)

--- leetcode-solutions/0234-palindrome-linked-list/solution.py
+++ leetcode-solutions/0234-palindrome-linked-list/solution.py
@@ -3,34 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        nums = []
+        slow, fast = head, head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
         
-        while head:
-            nums.append(head.val)
-            head = head.next
+        prev, curr = None, slow
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
         
-        l, r = 0, len(nums) - 1
-        
-        while l <= r:
-            if nums[l] != nums[r]:
+        while head and prev:
+            if head.val != prev.val:
                 return False
-            
-            l+=1
-            r-=1
+            head = head.next
+            prev = prev.next
+                
         return True
-        
-        
-        
-        # First create an empty array
-        # While head is not None
-        # Append the value of head to the array
-        # Move the head to head.next
-        # Now we have array of values of the link list
-        # Declare left as 0 and right as length of array - 1
-        # Using two pointer approach move from both end of the array by checking values are same or different
-        # If it different break the loop
-        # Return left >= Right
-        
