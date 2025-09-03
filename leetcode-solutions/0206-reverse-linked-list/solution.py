d3068e316c2444c9e3d173058152f184dd380bbb
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-09-03 23:42:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.8 MB (37.99%)

--- leetcode-solutions/0206-reverse-linked-list/solution.py
+++ leetcode-solutions/0206-reverse-linked-list/solution.py
@@ -8,9 +8,9 @@ class Solution:
         prev, curr = None, head
 
         while curr:
-            nxt = curr.next
+            temp = curr.next
             curr.next = prev
             prev = curr
-            curr = nxt
+            curr = temp
         
         return prev
