6603b907ad165b740f7634cca3bfd634ac4bed9b
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-09-05 05:43:38 +0000

    [LeetCode Sync] Runtime - 84 ms (33.98%), Memory - 20.3 MB (37.76%)

--- leetcode-solutions/1046-max-consecutive-ones-iii/solution.py
+++ leetcode-solutions/1046-max-consecutive-ones-iii/solution.py
@@ -1,22 +1,20 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        maxx = 0 # track of max number of consecutive 1's
-        num_zeros = 0
-        
+        # sliding window, use R to expand and L to contract the window
         l = 0
+        maxW = 0
+        numZeros = 0
+
         for r in range(len(nums)):
-            if nums[r] == 0: # flip to 1 to test maxx?
-                num_zeros += 1
-            
-            while num_zeros > k:
-                if nums[l] == 0: # l's job to contract/shrink the window
-                    num_zeros -= 1
+            if nums[r] == 0:
+                numZeros += 1
+
+            while numZeros > k:
+                if nums[l] == 0:
+                    numZeros -= 1
                 l += 1
-
-            window = ((r - l) + 1)
-            maxx = max(maxx, window) # update maxx after window resizing
+            
+            w = r - l + 1
+            maxW = max(w, maxW)
         
-        return maxx
-
-        # time: 
-        # space: 
+        return maxW
