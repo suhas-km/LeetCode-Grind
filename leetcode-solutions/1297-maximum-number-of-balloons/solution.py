28444d2e92bfbf8a6b1b964c52799f3502bb3f64
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-09-12 00:06:33 +0000

    [LeetCode Sync] Runtime - 1 ms (72.05%), Memory - 17.7 MB (94.24%)

---
+++ leetcode-solutions/1297-maximum-number-of-balloons/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        countText = Counter(text)
+        balloon = Counter("balloon")
+        res = len(text) # or float("inf")
+
+        for c in balloon:
+            res = min(res, countText[c] // balloon[c])
+        
+        return res
