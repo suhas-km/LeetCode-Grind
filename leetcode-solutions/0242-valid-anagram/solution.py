703c670f98dfe9c351f7f1f34531387b342b8ce5
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-09-15 22:14:09 +0000

    [LeetCode Sync] Runtime - 7 ms (89.17%), Memory - 18 MB (34.14%)

--- leetcode-solutions/0242-valid-anagram/solution.py
+++ leetcode-solutions/0242-valid-anagram/solution.py
@@ -1,21 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # hm = {}
-        # hm2 = {}
-
-        # for i in s:
-        #     if i not in hm:
-        #         hm[i] = 1
-        #     hm[i] += 1
-
-        # for j in t:
-        #     if j not in hm2:
-        #         hm2[j] = 1
-        #     hm2[j] += 1
-
-        # return hm == hm2
-        
-        sCounter = Counter(s)
-        tCounter = Counter(t)
-
-        return sCounter == tCounter
+        return Counter(s) == Counter(t)
