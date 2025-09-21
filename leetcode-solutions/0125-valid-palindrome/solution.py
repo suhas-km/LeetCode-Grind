527d9673c73c2279a364bc1553d3f77c73fa8d3c
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-09-21 21:31:51 +0000

    [LeetCode Sync] Runtime - 3 ms (98.95%), Memory - 17.8 MB (99.34%)

--- leetcode-solutions/0125-valid-palindrome/solution.py
+++ leetcode-solutions/0125-valid-palindrome/solution.py
@@ -3,21 +3,18 @@ class Solution:
         l, r = 0, len(s) - 1
 
         while l < r:
-            # Skip non-alphanumeric from left
             if not s[l].isalnum():
                 l += 1
                 continue
-
-            # Skip non-alphanumeric from right
+            
             if not s[r].isalnum():
                 r -= 1
                 continue
-                
-            # Compare after converting to lowercase
+
             if s[l].lower() != s[r].lower():
                 return False
-
-            l += 1
+            
+            l += 1 # keep moving and checking
             r -= 1
         
         return True
