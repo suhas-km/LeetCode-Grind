317f5c26b65814891490a6c7430463a109f45529
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-10-12 23:15:35 +0000

    [LeetCode Sync] Runtime - 23 ms (18.91%), Memory - 22.8 MB (11.80%)

--- leetcode-solutions/0049-group-anagrams/solution.py
+++ leetcode-solutions/0049-group-anagrams/solution.py
@@ -1,15 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # a hashmmap to group similar keys 
+        # keys - alphabeical represntation of the word we are currently seeing
         anagrams = defaultdict(list)
         res = []
 
         for word in strs:
-            key = [0] * 26 #basically key for every word
+            key = [0] * 26
             for ch in word:
-                key[ord(ch) - ord("a")] += 1 # creating the key properly for thew word
-            # appendd to anagrams
+                key[ord(ch) - ord("a")] += 1
+
             anagrams[tuple(key)].append(word)
-                
+
         for _, value in anagrams.items():
             res.append(value)
         
