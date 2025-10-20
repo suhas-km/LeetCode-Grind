152908de145a4f0f47016a9db20f080ef44c651c
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-10-20 00:49:02 +0000

    [LeetCode Sync] Runtime - 5 ms (60.25%), Memory - 21.2 MB (89.36%)

--- leetcode-solutions/0155-min-stack/solution.py
+++ leetcode-solutions/0155-min-stack/solution.py
@@ -1,38 +1,22 @@
 class MinStack:
+
     def __init__(self):
-        # monotonically decreasing stack problem
-        # min_stack to be used to maintain the monotonically decreasing stack in order
         self.stack = []
         self.min_stack = []
 
-    def push(self, val):
+    def push(self, val: int) -> None:
         self.stack.append(val)
+
         if not self.min_stack or val <= self.min_stack[-1]:
             self.min_stack.append(val)
 
-    def pop(self):
+    def pop(self) -> None:
         popped = self.stack.pop()
         if popped == self.min_stack[-1]:
             self.min_stack.pop()
-        return popped
 
-    def top(self):
+    def top(self) -> int:
         return self.stack[-1]
 
-    def getMin(self):
+    def getMin(self) -> int:
         return self.min_stack[-1]
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
