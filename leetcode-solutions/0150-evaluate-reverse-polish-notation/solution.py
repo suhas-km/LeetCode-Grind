8f9c5d1662f5e2eb68a03e78a2737763de71c7f5
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-10-20 01:15:00 +0000

    [LeetCode Sync] Runtime - 3 ms (63.78%), Memory - 18.9 MB (97.61%)

--- leetcode-solutions/0150-evaluate-reverse-polish-notation/solution.py
+++ leetcode-solutions/0150-evaluate-reverse-polish-notation/solution.py
@@ -2,20 +2,24 @@ from typing import List
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        # stack
         stack = []
-        for c in tokens:
-            if c == '+':
-                stack.append(stack.pop() + stack.pop())
-            elif c == '-':
-                a, b = stack.pop(), stack.pop()
-                stack.append(b - a)
-            elif c == '*':
-                stack.append(stack.pop() * stack.pop())
-            elif c == '/':
-                a, b = stack.pop(), stack.pop()
-                stack.append(int(b / a))          # truncate toward 0
-            else:
-                stack.append(int(c))
-        return stack[0]
 
+        for token in tokens:
+            if token not in {'+', '-', '*', '/'}:
+                stack.append(int(token))
+                
+            else:
+                num1 = stack.pop()
+                num2 = stack.pop()
+                if token == '+':
+                    result = num2 + num1
+                elif token == '-':
+                    result = num2 - num1
+                elif token == '*':
+                    result = num2 * num1
+                else:  # token == '/'
+                    # Use int() to truncate toward zero (required by problem)
+                    result = int(num2 / num1)
+                stack.append(result)
+        
+        return stack[0]
