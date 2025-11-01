0cddfe98317b1d7527723aa3c664a05908cc5c32
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-01 22:51:27 +0000

    [LeetCode Sync] Runtime - 151 ms (77.45%), Memory - 19.1 MB (38.80%)

--- leetcode-solutions/0907-koko-eating-bananas/solution.py
+++ leetcode-solutions/0907-koko-eating-bananas/solution.py
@@ -1,23 +1,22 @@
-# Problem: Find the minimum eating speed 'k' such that all bananas in 'piles' can be eaten within 'h' hours.
-# Approach: Binary search over possible speeds (1 to max pile), and simulate time taken for each guess.
-
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles) # range of possible "k"
+        
+        l, r = 1, max(piles)
         res = r
 
         while l <= r:
             m = (l + r) // 2
-            timeTaken = 0
+            totalTime = 0
 
+            # calculate how many hours it takes to complete piles
             for pile in piles:
-                timeTaken += math.ceil(pile/m)    # calculate time taken for each pile
+                totalTime += math.ceil(pile/m)
             
-            if timeTaken <= h:
-                res = min(m, res)
-                r = m - 1
-
-            else:
+            if totalTime > h:
                 l = m + 1
             
+            elif totalTime <= h:
+                r = m - 1
+                res = m
+        
         return res
