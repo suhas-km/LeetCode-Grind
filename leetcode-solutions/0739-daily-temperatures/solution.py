259615b5f3f3ce56653c65fcf95138cfb9f42a0c
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-12 23:57:00 +0000

    [LeetCode Sync] Runtime - 131 ms (26.13%), Memory - 34.8 MB (12.40%)

--- leetcode-solutions/0739-daily-temperatures/solution.py
+++ leetcode-solutions/0739-daily-temperatures/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # # monotonic decreasing order - retriving a result in linear time!
-        
-        answer = [0] * len(temperatures)
+        res = [0] * len(temperatures)
         stack = []
-
-        for i, t in enumerate(temperatures):
-            while stack and t > stack[-1][0]:
-                stackT, stackInd = stack.pop()
-                answer[stackInd] = i - stackInd
-            stack.append([t, i])
         
-        return answer
+        for i, v in enumerate(temperatures):
+            while stack and v > stack[-1][1]:
+                index, value = stack.pop()
+                res[index] = abs(i - index)
+
+            # append to stack if not
+            stack.append([i, v])
+        
+        return res
+
