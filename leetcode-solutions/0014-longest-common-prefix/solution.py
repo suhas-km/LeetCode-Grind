8fae39ad49941077b695bb8dcfb4cba4aed107b4
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-13 00:09:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (30.00%)

--- leetcode-solutions/0014-longest-common-prefix/solution.py
+++ leetcode-solutions/0014-longest-common-prefix/solution.py
@@ -1,21 +1,14 @@
-from typing import List
-
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        
-        minLen = float("inf")
+        res = strs[0]
+        prefixLen = len(res)
 
-        for s in strs:
-            if len(s) < minLen:
-                minLen = len(s)
-        
-        i = 0
-        while i < minLen:
-            for s in strs:
-                if s[i] != strs[0][i]:
-                    return strs[0][:i]
-            i += 1
-        
-        # edge case: Empty result
-        return strs[0][:i]
+        for string in strs[1:]:
+            while string[0:prefixLen] != res:
+                prefixLen -= 1
+                if prefixLen == 0:
+                    return ""
+                
+                res = res[0:prefixLen]
 
+        return res
