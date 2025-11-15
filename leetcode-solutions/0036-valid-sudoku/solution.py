150adda24ba8a1e7416beaa44f14bf0e40efa5b6
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-15 01:05:13 +0000

    [LeetCode Sync] Runtime - 7 ms (35.99%), Memory - 17.7 MB (92.14%)

--- leetcode-solutions/0036-valid-sudoku/solution.py
+++ leetcode-solutions/0036-valid-sudoku/solution.py
@@ -1,9 +1,21 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        res = []
-        for i in range(9):
-            for j in range(9):
-                element = board[i][j]
-                if element != '.':
-                    res += [(i, element), (element, j), (i // 3, j // 3, element)]
-        return len(res) == len(set(res))
+        gridCheck = defaultdict(set)
+        rowCheck = defaultdict(set)
+        colCheck = defaultdict(set)
+
+        ROWS, COLS = len(board), len(board[0])
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c].isdigit():
+                    val = board[r][c]
+
+                    if val in rowCheck[r] or val in colCheck[c] or val in gridCheck[(r // 3, c // 3)]:
+                        return False
+
+                    rowCheck[r].add(val)
+                    colCheck[c].add(val)
+                    gridCheck[(r // 3, c // 3)].add(val)
+        
+        return True
