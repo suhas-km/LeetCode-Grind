2ba5f53c6438b96e503cd6f5b37e5b7de6c11173
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-20 19:42:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.3 MB (24.92%)

--- leetcode-solutions/0074-search-a-2d-matrix/solution.py
+++ leetcode-solutions/0074-search-a-2d-matrix/solution.py
@@ -1,32 +1,35 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # Since the numbers are sorted we can perform binary search
-        # 2 passes: 1st to find the right row to search and next to find if target is there
-        # pass 1: O(log(m))
-        # pass 2: O(log(n))
-        # total time complexity: O(log(m + n))
+        # binary search on the rows first
 
         ROWS, COLS = len(matrix), len(matrix[0])
-        
-        top, bot = 0, ROWS - 1
-        while top <= bot:
-            row = (top + bot) // 2
-            if target > matrix[row][-1]:
-                top = row + 1
-            elif target < matrix[row][0]:
-                bot = row -1
-            else:
-                break
-        
-        if not (top <= bot): return False
+        topRow, botRow = 0, ROWS - 1
 
+        # 
+        while topRow <= botRow:
+            midRow = (topRow + botRow) // 2
+
+            if target > matrix[midRow][-1]:
+                topRow = midRow + 1
+
+            elif target < matrix[midRow][0]:
+                botRow = midRow - 1
+            
+            else:
+                # found the row
+                break
+            
+        row = (topRow + botRow) // 2
         l, r = 0, COLS - 1
+
         while l <= r:
             m = (l + r) // 2
+
             if target > matrix[row][m]:
                 l = m + 1
             elif target < matrix[row][m]:
                 r = m - 1
+            
             else:
                 return True
         
