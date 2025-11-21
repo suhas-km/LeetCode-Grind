59f8ae2e25e82a987b21c96d2b44db2504d90a28
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-21 16:39:50 +0000

    [LeetCode Sync] Runtime - 27 ms (23.47%), Memory - 17.9 MB (34.04%)

--- leetcode-solutions/2186-count-vowel-substrings-of-a-string/solution.py
+++ leetcode-solutions/2186-count-vowel-substrings-of-a-string/solution.py
@@ -1,19 +1,17 @@
 class Solution:
     def countVowelSubstrings(self, word: str) -> int:
-        j = 0
-        count = 0
         vowels = {"a", "e", "i", "o", "u"}
-        
+        count = 0
+        j = 0
+
         for i in range(len(word)):
-            vowelCount = defaultdict(int)
-            j = i
-            
+            vowelCount = defaultdict(int) # because ne wslide starts
+            j = i # use j to keep loop in bound
+
             while j < len(word) and word[j] in vowels:
                 vowelCount[word[j]] += 1
                 j += 1
                 if len(vowelCount) == 5:
                     count += 1
-        
+                    
         return count
-
-
