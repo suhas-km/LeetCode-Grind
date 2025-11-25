20221c973c93d9eb4499756b5a10f5f8292b94c4
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-25 19:52:45 +0000

    [LeetCode Sync] Runtime - 3 ms (41.75%), Memory - 17.8 MB (60.79%)

--- leetcode-solutions/0202-happy-number/solution.py
+++ leetcode-solutions/0202-happy-number/solution.py
@@ -1,27 +1,23 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        # use set() to keep track and findout if the sum of squares of digit
+        visited = set()
 
-        # slow and fast also we can do
-
-        visit = set()
-
-        while n not in visit:
-            visit.add(n)
+        while n not in visited:
+            visited.add(n)
             n = self.sumOfSquares(n)
 
             if n == 1:
                 return True
-        
+            
         return False
 
     def sumOfSquares(self, n: int) -> int:
-        output = 0
+        new_n = 0
 
         while n:
             digit = n % 10
-            digit = digit ** 2
-            output += digit
+            digit = digit **2
+            new_n += digit
             n = n // 10
         
-        return output
+        return new_n
