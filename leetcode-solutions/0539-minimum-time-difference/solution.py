d6526ae59f4a31d0452210997fc28d1ce2c1cc0a
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-26 15:41:55 +0000

    [LeetCode Sync] Runtime - 27 ms (6.51%), Memory - 19.6 MB (85.93%)

--- leetcode-solutions/0539-minimum-time-difference/solution.py
+++ leetcode-solutions/0539-minimum-time-difference/solution.py
@@ -1,23 +1,19 @@
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
-        timePoints.sort()
+        timePoints.sort() #n* log n
 
         def time_to_min(t):
-            h, m = t.split(":")
-            h, m = int(h), int(m)
-            return 60 * h + m
+            h, m = map(int, t.split(":"))
+            total = h * 60 + m
+            return total
         
-        res = (
-            24 * 60 -
-            time_to_min(timePoints[-1]) +
-            time_to_min(timePoints[0])
-        ) # assigning it to maximum
+        # assign res to max:
+        res = (24 * 60 - time_to_min(timePoints[-1]) + time_to_min(timePoints[0]))
 
-        for i in range(len(timePoints) - 1):
-            cur = time_to_min(timePoints[i + 1])
-            prev = time_to_min(timePoints[i])
+        for time in range(len(timePoints) - 1):
+            cur = time_to_min(timePoints[time + 1])
+            prev = time_to_min(timePoints[time])
             diff = cur - prev
             res = min(res, diff)
-            
+        
         return res
-
