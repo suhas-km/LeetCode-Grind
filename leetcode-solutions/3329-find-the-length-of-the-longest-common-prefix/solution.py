0ae989bcaec45135243fc4e4108ad48f182d8f1c
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-26 15:21:26 +0000

    [LeetCode Sync] Runtime - 498 ms (27.11%), Memory - 28.7 MB (90.21%)

--- leetcode-solutions/3329-find-the-length-of-the-longest-common-prefix/solution.py
+++ leetcode-solutions/3329-find-the-length-of-the-longest-common-prefix/solution.py
@@ -1,22 +1,22 @@
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        # pick the smaller len
-        if len(arr1) > len(arr2):
+        if len(arr1) > len(arr2): # pick the smaller list
             arr1, arr2 = arr2, arr1
 
         prefix_set = set()
         for n in arr1:
-            while n and n not in prefix_set: # while non-zero
+            while n and n not in prefix_set: # non-zero and not in set
                 prefix_set.add(n)
                 n = n // 10
         
-        res = 0
-        for n in arr2:
-            while n and n not in prefix_set: # while n is non-zero and n not in prefixSet
-                n = n //10
-            if n != 0:
-                res = max(res, len(str(n)))
+        res = 0 
+        for num in arr2:
+            while num and num not in prefix_set: # non-zero and not in set
+                print(num)
+                num = num // 10
+            
+            if num != 0:
+                res = max(res, len(str(num)))
         
         return res
 
-        # time: O((n.log) * (m.log))
