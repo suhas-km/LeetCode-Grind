b57afdefae8436117c09eb17f15d7f3389694ca3
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-27 15:55:01 +0000

    [LeetCode Sync] Runtime - 88 ms (69.72%), Memory - 49.8 MB (80.07%)

--- leetcode-solutions/2779-number-of-adjacent-elements-with-the-same-color/solution.py
+++ leetcode-solutions/2779-number-of-adjacent-elements-with-the-same-color/solution.py
@@ -8,16 +8,16 @@ class Solution:
             left = colors[idx - 1] if idx > 0 else 0
             right = colors[idx + 1] if idx < len(colors) - 1 else 0
 
-            if colors[idx] != 0 and colors[idx] == left:
-                adjPairs -= 1
             if colors[idx] != 0 and colors[idx] == right:
                 adjPairs -= 1
-            
+            if colors[idx] != 0 and colors[idx] == left:
+                adjPairs -= 1
+
             colors[idx] = color
 
-            if colors[idx] == left:
-                adjPairs += 1
             if colors[idx] == right:
+                adjPairs += 1
+            if colors[idx] == left:
                 adjPairs += 1
             
             res.append(adjPairs)
