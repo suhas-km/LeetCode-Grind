5d647e7be057696e8f1be62cffbe2119f89e07f4
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-27 02:53:18 +0000

    [LeetCode Sync] Runtime - 83 ms (85.08%), Memory - 50 MB (63.78%)

---
+++ leetcode-solutions/2779-number-of-adjacent-elements-with-the-same-color/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def colorTheArray(self, n: int, queries: List[List[int]]) -> List[int]:
+        colors = [0] * n
+        adjPairs = 0
+        res = []
+
+        for idx, color in queries:
+            left = colors[idx - 1] if idx > 0 else 0
+            right = colors[idx + 1] if idx < len(colors) - 1 else 0
+
+            if colors[idx] != 0 and colors[idx] == left:
+                adjPairs -= 1
+            if colors[idx] != 0 and colors[idx] == right:
+                adjPairs -= 1
+            
+            colors[idx] = color
+
+            if colors[idx] == left:
+                adjPairs += 1
+            if colors[idx] == right:
+                adjPairs += 1
+            
+            res.append(adjPairs)
+        
+        return res
