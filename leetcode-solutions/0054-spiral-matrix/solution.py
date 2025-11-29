b6be60e4d7eefa4761aa2e565eeb99cf84e074b2
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-29 17:28:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (18.05%)

--- leetcode-solutions/0054-spiral-matrix/solution.py
+++ leetcode-solutions/0054-spiral-matrix/solution.py
@@ -1,29 +1,27 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         res = []
+        top, bot = 0, len(matrix)
         left, right = 0, len(matrix[0])
-        top, bottom = 0, len(matrix)
 
-        while left < right and top < bottom:
-            #get every i in the top row
+        while top < bot and left < right:
             for i in range(left, right):
                 res.append(matrix[top][i])
             top += 1
 
-            #get every i in the right col
-            for i in range(top, bottom):
+            for i in range(top, bot):
                 res.append(matrix[i][right - 1])
             right -= 1
 
-            if not (left < right and top < bottom):
+            if not (top < bot and left < right):
                 break
             
             for i in range(right - 1, left - 1, -1):
-                res.append(matrix[bottom - 1][i])
-            bottom -= 1
-
-            for i in range(bottom - 1, top - 1, - 1):
+                res.append(matrix[bot - 1][i])
+            bot -= 1
+            
+            for i in range(bot - 1, top - 1, -1):
                 res.append(matrix[i][left])
             left += 1
-        
+
         return res
