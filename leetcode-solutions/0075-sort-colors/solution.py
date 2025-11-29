5056c4fe9f80018046c448c9930ec7c9ea9feb8e
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-29 16:13:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (53.36%)

--- leetcode-solutions/0075-sort-colors/solution.py
+++ leetcode-solutions/0075-sort-colors/solution.py
@@ -3,22 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l, r = 0, len(nums) - 1
+        l, r = 0 , len(nums) - 1
         i = 0
 
         def swap(i, j):
             temp = nums[i]
             nums[i] = nums[j]
             nums[j] = temp
-
-        while i <= r:
+            
+        while  i <= r:
             if nums[i] == 0:
-                swap(l, i)
+                swap(i, l)
                 l += 1
             
-            elif nums[i] == 2:
-                swap(i, r)
+            if nums[i] == 2:
+                swap(r, i)
                 r -= 1
                 i -= 1
-
+            
             i += 1
+
