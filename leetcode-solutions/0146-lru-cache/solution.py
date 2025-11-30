3a793bf7078c1b83effad8d5b83a56839badc575
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-11-30 01:27:55 +0000

    [LeetCode Sync] Runtime - 153 ms (38.59%), Memory - 78 MB (42.98%)

--- leetcode-solutions/0146-lru-cache/solution.py
+++ leetcode-solutions/0146-lru-cache/solution.py
@@ -1,60 +1,57 @@
 class ListNode:
-    def __init__(self, key: int, value: int):
-        self.key = key
-        self.value = value
-        self.prev = None
-        self.next = None
+    def __init__(self, key, value):
+        self.key, self.value = key, value
+        self.prev = self.next = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.cache = {}  # key -> ListNode
+        self.cache = {}
 
-        # Dummy head (LRU side) and tail (MRU side)
+        # set dummy nodes and link them up
         self.head = ListNode(0, 0)
         self.tail = ListNode(0, 0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        self.head.next, self.tail.prev = self.tail, self.head
 
-    # Remove an existing node from the list
-    def remove(self, node: ListNode) -> None:
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-
-    # Insert a node at the right (just before tail) -> most recently used
-    def insert(self, node: ListNode) -> None:
+    def insert(self, node: ListNode):
+        # inserting at Tail
         prev, nxt = self.tail.prev, self.tail
         prev.next = node
         node.prev = prev
         node.next = nxt
         nxt.prev = node
 
+    def remove(self, node: ListNode):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
 
         node = self.cache[key]
-        # Move node to the MRU position
         self.remove(node)
         self.insert(node)
         return node.value
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            # remove old node from list
             self.remove(self.cache[key])
 
-        # create / update node and move to MRU position
         node = ListNode(key, value)
         self.cache[key] = node
         self.insert(node)
 
         if len(self.cache) > self.cap:
-            # evict LRU from the left (just after head)
+            # evict process starts
             lru = self.head.next
             self.remove(lru)
             del self.cache[lru.key]
+
+        
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
