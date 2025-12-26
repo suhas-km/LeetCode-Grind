685b2689e21ca1162b58e7de0a54ad24d67568fe
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2025-12-26 18:45:43 +0000

    [LeetCode Sync] Runtime - 3 ms (33.60%), Memory - 17.4 MB (96.43%)

---
+++ leetcode-solutions/0046-permutations/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        # 2 dynamic arrays
+        ans, sol = [], []
+        
+        def backtrack():
+            if len(sol) == n:
+                ans.append(sol[:])
+                return
+            
+            for x in nums:
+                if x not in sol:
+                    sol.append(x)
+                    backtrack()
+                    sol.pop()
+        
+        backtrack()
+        return ans
