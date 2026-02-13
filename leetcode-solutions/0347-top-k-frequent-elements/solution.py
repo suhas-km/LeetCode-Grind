2820294c1d45267e8c2de2633d2679e8a769ec67
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2026-02-13 20:21:49 +0000

    [LeetCode Sync] Runtime - 4 ms (70.73%), Memory - 25.1 MB (5.18%)

--- leetcode-solutions/0347-top-k-frequent-elements/solution.py
+++ leetcode-solutions/0347-top-k-frequent-elements/solution.py
@@ -1,22 +1,20 @@
+from collections import Counter
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hm = defaultdict(int)
+        countMap = Counter(nums)
 
-        for num in nums:
-            hm[num] += 1
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        for val, freq in countMap.items():
+            buckets[freq].append(val)
         
         res = []
-
-        hm2 = defaultdict(list)
-        for i, val in hm.items():
-            hm2[val].append(i)
-        
-        for i in range(len(nums), -1, -1):
-            if i in hm2:
-                for element in hm2[i]:
-                    res.append(element)
-                    if len(res) == k:
-                        return res
-
-        return res
-
+        for i in range(len(buckets) - 1, 0, -1):
+            for n in buckets[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+                    
+        # time: O(n), even though k could be the sizes individual lists inside the bucket, its max value can still only be n(input size)
+        # space: O(n)
