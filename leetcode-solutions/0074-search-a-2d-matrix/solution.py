b27a15a568c315e1af065523e8531c05a8d2041c
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2026-02-14 20:50:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (51.35%)

--- leetcode-solutions/0074-search-a-2d-matrix/solution.py
+++ leetcode-solutions/0074-search-a-2d-matrix/solution.py
@@ -1,36 +1,34 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # binary search on the rows first
-
         ROWS, COLS = len(matrix), len(matrix[0])
         topRow, botRow = 0, ROWS - 1
 
-        # 
         while topRow <= botRow:
             midRow = (topRow + botRow) // 2
 
             if target > matrix[midRow][-1]:
                 topRow = midRow + 1
-
+            
             elif target < matrix[midRow][0]:
                 botRow = midRow - 1
             
             else:
-                # found the row
                 break
-            
+        
         row = (topRow + botRow) // 2
-        l, r = 0, COLS - 1
+        l, r = 0, COLS -1
 
         while l <= r:
             m = (l + r) // 2
 
             if target > matrix[row][m]:
                 l = m + 1
+            
             elif target < matrix[row][m]:
                 r = m - 1
             
             else:
                 return True
-        
+
         return False
+
