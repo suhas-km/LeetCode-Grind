df6152c9e1a68710ad447622379905a65edb8771
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2026-02-14 04:26:07 +0000

    [LeetCode Sync] Runtime - 1559 ms (7.90%), Memory - 23.6 MB (6.85%)

--- leetcode-solutions/0015-3sum/solution.py
+++ leetcode-solutions/0015-3sum/solution.py
@@ -1,25 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        res = set()
         nums.sort()
 
-        for i, a in enumerate(nums):
-            if i > 0 and nums[i] == nums[i-1]: # skip logic
-                continue
-
+        for i in range(len(nums)):
+            # if i > 0 and nums[i] == nums[i - 1]:
+            #     continue
+            
             l, r = i + 1, len(nums) - 1
-
             while l < r:
-                threeSum = a + nums[l] + nums[r]
-
-                if threeSum > 0:
+                total = nums[i] + nums[r] + nums[l]
+                if total == 0:
+                    res.add((nums[i], nums[l], nums[r]))
+                    l += 1
                     r -= 1
-                elif threeSum < 0:
+                
+                elif total < 0:
                     l += 1
+                
                 else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l-1] and l<r:
-                        l+=1
+                    r -= 1
+        
+        return [list(x) for x in res]
 
-        return res
