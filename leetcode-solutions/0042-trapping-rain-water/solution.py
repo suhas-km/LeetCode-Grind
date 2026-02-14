236dd5df3b4059dd6c6ef151d31c8ced19d74a14
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2026-02-14 17:20:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21 MB (56.38%)

--- leetcode-solutions/0042-trapping-rain-water/solution.py
+++ leetcode-solutions/0042-trapping-rain-water/solution.py
@@ -1,10 +1,9 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         if not height: return None
-
         l, r = 0, len(height) - 1
-        maxL, maxR = height[l], height[r]
         res = 0
+        maxL, maxR = height[l], height[r]
 
         while l < r:
             if maxL < maxR:
@@ -16,5 +15,6 @@ class Solution:
                 r -= 1
                 maxR = max(maxR, height[r])
                 res += maxR - height[r]
-            
+        
         return res
+
