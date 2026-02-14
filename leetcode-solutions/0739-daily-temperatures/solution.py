ed9cfbdad499c1cc85a08033295a149f8edec9ae
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2026-02-14 19:19:07 +0000

    [LeetCode Sync] Runtime - 132 ms (16.82%), Memory - 35.7 MB (8.25%)

--- leetcode-solutions/0739-daily-temperatures/solution.py
+++ leetcode-solutions/0739-daily-temperatures/solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # Is this monotonically decreasing or increasing?
         res = [0] * len(temperatures)
         stack = []
-        
+
         for i, v in enumerate(temperatures):
             while stack and v > stack[-1][1]:
                 index, value = stack.pop()
                 res[index] = abs(i - index)
-
-            # append to stack if not
+            
             stack.append([i, v])
         
         return res
-
+        
