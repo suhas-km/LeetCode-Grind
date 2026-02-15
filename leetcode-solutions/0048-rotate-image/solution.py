b7b018c4d5424c6d57ccfbd0efdf5acc98de122a
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2026-02-15 16:06:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (48.44%)

--- leetcode-solutions/0048-rotate-image/solution.py
+++ leetcode-solutions/0048-rotate-image/solution.py
@@ -3,17 +3,25 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
         ROWS, COLS = len(matrix), len(matrix[0])
 
-        # transpose
+        # transpose starts
+        # We only swap elements above the diagonal
+        # c starts at r + 1 so we skip:
+        # c == r (diagonal) because (r,r) swapping with itself is pointless
+        # c < r (below diagonal) because those swaps would be duplicates
         for r in range(ROWS):
             for c in range(r + 1, COLS):
                 matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
         
-        # reflect
+        # For each row, you swap left side with right side.
+        # You only go up to n//2 because once you’ve swapped the first half, the second half is already done.
+        # In general: mirror = last_index − column you are on
+
         for r in range(ROWS):
             for c in range(COLS // 2):
-                matrix[r][c], matrix[r][(COLS -1) - c] = matrix[r][(COLS -1) - c], matrix[r][c]
-        # transpose + reflect
+                matrix[r][c], matrix[r][(COLS - 1) - c] = matrix[r][(COLS - 1) - c], matrix[r][c]
+        
         return matrix
+        
+
