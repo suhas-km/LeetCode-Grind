c648d78a63e2b175ee0792064e2759662f0d25af
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2026-02-15 01:39:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (51.56%)

--- leetcode-solutions/0021-merge-two-sorted-lists/solution.py
+++ leetcode-solutions/0021-merge-two-sorted-lists/solution.py
@@ -3,28 +3,28 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # use merge sort logic similar
-        
         curr = dummy = ListNode()
-        
+
         while list1 and list2:
             if list1.val < list2.val:
-                curr.next = list1
+                dummy.next = list1
                 list1 = list1.next
+                dummy = dummy.next
             
             else:
-                curr.next = list2
+                dummy.next = list2
                 list2 = list2.next
-            
-            curr = curr.next
+                dummy = dummy.next
         
-        # final edge case
+            # dummy = dummy.next
+        
         if list1:
-            curr.next = list1
-        elif list2:
-            curr.next = list2
+            dummy.next = list1
+        if list2: 
+            dummy.next = list2
         
-        return dummy.next
+        return curr.next
 
