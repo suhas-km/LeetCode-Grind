783735f3545c43ede7cc5030af1ed1f77901ebe6
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2026-02-15 16:18:03 +0000

    [LeetCode Sync] Runtime - 3 ms (69.56%), Memory - 20 MB (63.66%)

--- leetcode-solutions/0073-set-matrix-zeroes/solution.py
+++ leetcode-solutions/0073-set-matrix-zeroes/solution.py
@@ -4,13 +4,11 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         ROWS, COLS = len(matrix), len(matrix[0])
-
         flaggedRow = set()
         flaggedCol = set()
 
         for r in range(ROWS):
             for c in range(COLS):
-
                 if matrix[r][c] == 0:
                     flaggedRow.add(r)
                     flaggedCol.add(c)
@@ -20,4 +18,3 @@ class Solution:
                 if r in flaggedRow or c in flaggedCol:
                     matrix[r][c] = 0
         
-
