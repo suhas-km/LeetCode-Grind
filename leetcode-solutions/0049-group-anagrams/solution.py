44fd56374ffbf2a8668e06eac9b1aa3b13f423d4
Suhas K M <145060299+suhaskm-neu@users.noreply.github.com>
2026-02-17 20:57:08 +0000

    [LeetCode Sync] Runtime - 20 ms (18.08%), Memory - 24.1 MB (14.31%)

--- leetcode-solutions/0049-group-anagrams/solution.py
+++ leetcode-solutions/0049-group-anagrams/solution.py
@@ -1,37 +1,17 @@
-from collections import defaultdict
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = defaultdict(list)
 
-        # SORTING APPROACH
-        # for word in strs:
-        #     sorted_key = "".join(sorted(word))
-        #     seen[sorted_key].append(word)
-        
-        # return list(seen.values())
-
-        # time: n. K logk, where k is the total number of strings in the strs array
-        # space: n.k, to strore both groups in the hashmap
-
-        # [1, 0, 0, 0, 1, .... 1, 0000] -> len -> 26
-        
-
-
         for word in strs:
             key = [0] * 26
-            # logic to prepare the key
             for ch in word:
                 key[ord(ch) - ord('a')] += 1
-
-            anagrams[tuple(key)].append(word) # cant store list as key in hashmap
+            
+            anagrams[tuple(key)].append(word)
         
         res = []
         for key, value in anagrams.items():
             res.append(value)
         
         return res
-
-        # time: O (n * k), where k is the average length of each string
-        # space: O(n)
 
